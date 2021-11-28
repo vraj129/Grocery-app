9bead4eee8a233ba64c3cfd86d2786d1acc227cf
vraj129 <vrajpatelv@protonmail.com>
2021-11-28 14:54:05 +0530

    Uploaded

--- app/src/main/java/com/example/grocery/Adapter/LoginAdapter.java
+++ app/src/main/java/com/example/grocery/Adapter/LoginAdapter.java
@@ -1,5 +1,43 @@
 package com.example.grocery.Adapter;
 
-public class LoginAdapter {
+import android.content.Context;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.grocery.Fragments.LoginTabFragment;
+import com.example.grocery.Fragments.RegisTabFragment;
+
+public class LoginAdapter extends FragmentPagerAdapter {
+    private Context context;
+    int totalTabs;
+
+    public LoginAdapter( FragmentManager fm, Context context, int totalTabs) {
+        super(fm);
+        this.context = context;
+        this.totalTabs = totalTabs;
+    }
+
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                LoginTabFragment loginTabFragment = new LoginTabFragment();
+                return loginTabFragment;
+            case 1:
+                RegisTabFragment regisTabFragment = new RegisTabFragment();
+                return regisTabFragment;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
 }
--- app/src/main/java/com/example/grocery/LoginRegistration.java
+++ app/src/main/java/com/example/grocery/LoginRegistration.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
+import com.example.grocery.Adapter.LoginAdapter;
 import com.google.android.material.tabs.TabLayout;
 
 public class LoginRegistration extends AppCompatActivity {
@@ -15,12 +17,38 @@ public class LoginRegistration extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login_registration);
+        getSupportActionBar().hide();
         tabLayout = findViewById(R.id.tab_layout);
         viewPager = findViewById(R.id.view_pager);
 
         tabLayout.addTab(tabLayout.newTab().setText("Login"));
         tabLayout.addTab(tabLayout.newTab().setText("Register"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(),this,tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
     }
 }
